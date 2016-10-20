836a9f9d12a0f755a15fcd64d1d72d692734107b
Saugat321 <saugatk9@gmail.com>
2016-10-20 12:02:49 +0545

    Add files via upload
    Realation operator

---
+++ s.c
@@ -0,0 +1,40 @@
+/*
+Name:Saugat Karki
+Subject:Programming Fundamentals
+Roll no.:
+Program:WAP to use of assignment operator
+Lab no.:04
+Date:2016/10/20
+*/
+
+   #include<stdio.h>
+    int main(){
+    int a,b;
+
+    printf("Enter first number :\n");
+    scanf("%d",&a);
+    printf("Enter second number :\n");
+    scanf("%d",&b);
+
+
+
+    a==b;
+    printf("\n in use of==of %d",a);
+
+    a>b;
+    printf("\n in use of > of %d",a);
+
+    a<b;
+    printf("\n in use of < of %d",a);
+
+    a!=b;
+    printf("\n in use of !=of %d",a);
+
+    a>=b;
+    printf("\n in use of >= of %d",a);
+
+    a<=b;
+    printf("\n in use of <=of %d",a);
+    return 0;
+    }
+
