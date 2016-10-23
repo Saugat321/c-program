8d500c74c1ed936081fce09919a208660c447802
Saugat321 <saugatk9@gmail.com>
2016-10-23 11:03:20 +0545

    Add files via upload
    switch

---
+++ switch.c
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+#include<stdio.h>
+
+
+ int main(){
+      int a,b,res:
+      char operator;
+      
+      printf("\n Enter first number: ");
+      scanf("%d",&a);
+      printf("\n Enter second number: ");
+      scanf("%d",&b);
+      printf("select an operator (+,-,*,/)");
+      scanf("%c",&operator);
