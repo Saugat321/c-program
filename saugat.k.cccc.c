42cd655cd97ac685b8eaa582af9baccd203bf333
Saugat321 <saugatk9@gmail.com>
2016-11-15 10:25:47 +0545

    Add files via upload
    10 times name

---
+++ saugat.k.cccc.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Saugat karki
+Subject:Programming fundamental
+Program:WAP to print Name 10 times for initialization,condition & increment
+Roll no.:
+Lab sheet no.:14
+Date:15 november,2016
+*/
+
+
+   #include<stdio.h>
+
+
+   int main()
+   {
+   int i;
+
+   for(i=0; i<10; i++)
+   {
+    printf("\n %d= Saugat Karki",i);
+   }
+  return 0;
+   }
