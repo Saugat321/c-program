061903e6ef95f369a1e0b50140c180a0f5d05100
Saugat321 <saugatk9@gmail.com>
2017-01-03 10:58:43 +0545

    Add files via upload
    WAP to print ulto star

---
+++ sstar.c
@@ -0,0 +1,29 @@
+/*
+Name:Saugat Karki
+Subject:Programming fundamental
+Lab sheet 
+Roll No
+program
+Date=janaury 3,2017
+*/
+#include<stdio.h>
+  int main()
+  {
+  for(int i=1;i<=5;i++)
+  {
+  for(int j=1;j<=5;j++)
+  {
+	  if(i<=j){
+	  printf("*");
+  }
+  else
+  {
+  printf(" ");
+}
+
+
+}
+  printf("\n");
+  }
+  return 0;
+}
